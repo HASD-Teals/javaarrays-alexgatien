75753f05b1bd3ff4b01cda20e19be59cd2b1603e
alexgatien <alexgatien14@gmail.com>
2021-11-09 10:00:59 -0600

    Todays Work

---
+++ JavaArrays/src/WhatALife.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class WhatALife {
+    public static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+        int[] keyAge = new int[] {0,5,10,13,15,16,18,21,30,40,50,55,65,70,100};
+        String[] keyEvent = new String[] {"Birth", "Kindergarten", "Preteen", "Teen", "Driving",
+         "Sweet16", "High School Graduation", "Adult-All States", "Thirties", "Forties", "Fifties",
+         "Early Retirement", "Retirement", "Federal Retirement", "Centurian"};
+
+    }
+        public static void fullReport (){
+
+        }
+
+public static void keyAgetoYear (){
+    
+}
+
+
+
+
+
+
+
+}
+
